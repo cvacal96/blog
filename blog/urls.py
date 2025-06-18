0f1559b5c71de927cfea92bd1e7c9d5931e0a5db
cvacaloa <c.vaca_loaiza@dhl.com>
2025-06-18 15:20:06 -0500

    feat(infra)> change for new py version

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,15 @@
-from django.conf.urls import url
+from django.urls import re_path
 from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^login/$', auth_views.login, {'template_name': 'blog/login.html'}, name='login'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
-    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
+    re_path(r'^$', views.post_list, name='post_list'),
+
+    # Django 2.1+ uses 'LoginView.as_view()' instead of 'auth_views.login'
+    re_path(r'^login/$', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+
+    re_path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    re_path(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+    re_path(r'^post/new/$', views.post_new, name='post_new'),
 ]

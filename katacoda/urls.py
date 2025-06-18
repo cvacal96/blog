c3a65daf76b9893c5ecf03d08b8258841c13fb81
cvacaloa <c.vaca_loaiza@dhl.com>
2025-06-18 15:16:09 -0500

    feat(infra): make change of version katakoda

--- katacoda/urls.py
+++ katacoda/urls.py
@@ -1,32 +1,31 @@
 """katacoda URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+    https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  re_path(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  re_path(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import include, re_path
+    2. Add a URL to urlpatterns:  re_path(r'^blog/', include('blog.urls'))
 """
 
-from django.conf.urls import include, url
+from django.urls import include, re_path
 from django.contrib import admin
 from django.conf.urls.static import static
 
 from . import settings
-
 from . import healthz
 
 urlpatterns = [
-    url(r'^healthz/ready', healthz.ready),
-    url(r'^healthz/alive', healthz.alive),
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('blog.urls')),
+    re_path(r'^healthz/ready$', healthz.ready),
+    re_path(r'^healthz/alive$', healthz.alive),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'', include('blog.urls')),
 ]
 
 if settings.DEBUG:
